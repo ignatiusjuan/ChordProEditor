7d1bab5f6df20b04f5890059b2084dc91c9f6483
IgnatiusJuanPradipta <IgnatiusJuanPradipta@DESKTOP-QN320Q1>
2019-09-02 13:49:52 +0200

    .

---
+++ ChordProEditor/src/main/java/de/chordsystem/chordproeditor/model/classes/ChordChecker.java
@@ -0,0 +1,13 @@
+package de.chordsystem.chordproeditor.model.classes;
+
+public class ChordChecker {
+	private static final String chordPattern = "";
+	
+	public static boolean isAChord(String word) {
+		boolean isAChord = true;
+		
+		
+		
+		return isAChord;
+	}
+}
