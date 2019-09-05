6016bee8d65d9de91a2521ad971c3b217d68324a
IgnatiusJuanPradipta <IgnatiusJuanPradipta@DESKTOP-QN320Q1>
2019-09-05 10:24:30 +0200

    +chordchecker

--- ChordProEditor/src/main/java/de/chordsystem/chordproeditor/model/classes/ChordChecker.java
+++ ChordProEditor/src/main/java/de/chordsystem/chordproeditor/model/classes/ChordChecker.java
@@ -1,13 +1,23 @@
 package de.chordsystem.chordproeditor.model.classes;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class ChordChecker {
-	private static final String chordPattern = "";
+	private static final String chordPattern = "(C|D|E|F|G|A|B)(#|b)?((add|sus|M|m|min|man|aug|dim)?(0|2|4|5|6|7|9|11|13)?(#|b)?)*+/?(C|D|E|F|G|A|B)?(#|b)?";
 	
 	public static boolean isAChord(String word) {
 		boolean isAChord = true;
 		
-		
+		Pattern.compile(chordPattern).matcher(word).find();
+		Matcher m = Pattern.compile(chordPattern).matcher(word);
+		m.find();
+		System.out.println(m.group(0));
 		
 		return isAChord;
+	}
+	
+	public static void main(String[] args) {
+		isAChord("Bbsus7#9b9#11#5/Ab");
 	}
 }
